98d5957e53f760f2648ff10986cccbfa1a38530c
Antonio <antonioharoarrriaza2@gmail.com>
2023-11-17 11:42:18 +0100

    Muestra un boton con nombre y apellidos e imagen pero no funciona la navegacion

--- EducaParaTodo/src/Vista/eliminarTarea.jsx
+++ EducaParaTodo/src/Vista/eliminarTarea.jsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import appFirebase from '../Modelo/firebase';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { ScrollView } from 'react-native-web';
-import DatosAlumnos from './DatosAlumnos';
-import alumnos from '../Modelo/alumno';
+import { useNavigation } from '@react-navigation/native';
+//import DatosAlumnos from './DatosAlumnos';
+//import alumnos from '../Modelo/alumno';
 
 const TuComponente = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const alumnosArray = Object.values(data);
+  const navigation = useNavigation();
+
+  const handleItemPress = (item) =>  {
+    navigation.navigate('eliminarTareaAlumno', {item});
+  };
+
 
   useEffect(() => {
     const mostrarAlumnos = async () => {
@@ -41,10 +47,27 @@ const TuComponente = () => {
         <Text>Cargando...</Text>
       ) : (
         <ScrollView contentContainerStyle={styles.datos}>
-      {alumnosArray.map((alumno, index) => (
-        <View key={index} style={styles.elementoList}>
-           <DatosAlumnos alumno={alumno} navigation={navigation} />
-        </View>
+      {data.map((item, index) => (
+       /* <View key={index} style={styles.cardWithImage}>
+          {item.Imagen ? (
+              <Image source={{ uri: item.Imagen }} style={styles.image} />
+            ) : (
+              <View style={styles.image} />
+            )}
+           <Text style={{ fontSize: 18 }}>{item.nombre} {item.apellidos}</Text>
+        </View>*/
+        <TouchableOpacity
+      key={index}
+      onPress={() => handleItemPress(item)} // Llama a la función handleItemPress con el item como argumento al presionar
+      style={styles.cardWithImage}
+    >
+      {item.Imagen ? (
+        <Image source={{ uri: item.Imagen }} style={styles.image} />
+      ) : (
+        <View style={styles.image} />
+      )}
+      <Text style={{ fontSize: 18 }}>{item.nombre} {item.apellidos}</Text>
+    </TouchableOpacity>
       ))}
     </ScrollView>
       )}
@@ -53,6 +76,28 @@ const TuComponente = () => {
 };
 
 const styles = StyleSheet.create({
+  cardWithImage: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 20,
+    marginVertical: 8,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 20,
+  },
   datos: {
     flexDirection: 'row',
     flexWrap: 'wrap',
