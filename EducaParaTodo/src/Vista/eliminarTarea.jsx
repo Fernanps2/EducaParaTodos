834df21bfc822c1aa6982b80dda78aadafac9f8f
Antonio <antonioharoarrriaza2@gmail.com>
2023-11-17 10:09:05 +0100

    Cambios en eliminar Tarea, primera prueba, hace bien el formato de visualizacion pero no lo coge desde la base de datos

--- EducaParaTodo/src/Vista/eliminarTarea.jsx
+++ EducaParaTodo/src/Vista/eliminarTarea.jsx
@@ -1,89 +1,71 @@
 import React, { useState, useEffect } from 'react';
-import {Alert, View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import appFirebase from '../Modelo/firebase';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { ScrollView } from 'react-native-web';
+import DatosAlumnos from './DatosAlumnos';
+import alumnos from '../Modelo/alumno';
 
+const TuComponente = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const alumnosArray = Object.values(data);
 
-// Simulación de datos de tareas
-const mockTasks = [
-  { id: 1, title: 'Recoger la mesa' },
-  { id: 2, title: 'Hacer la comanda' },
-  { id: 3, title: 'Atarse los cordones' },
-];
+  useEffect(() => {
+    const mostrarAlumnos = async () => {
+      setIsLoading(true); // Iniciar la carga
+      try {
+        const querydb = getFirestore(appFirebase);
+        const queryCollection = collection(querydb, 'alumnos');
+        const querySnapshot = await getDocs(queryCollection);
 
-export default function EliminarTarea() {
-    const [tasks, setTasks] = useState(mockTasks);
-
-    const eliminarTarea = (id) => {
-      const updatedTasks = tasks.filter(task => task.id !== id);
-      setTasks(updatedTasks);
-    };
-  
-    const MostrarTarea = ({ item }) => (
-      <View style={styles.taskItem}>
-        <Text>{item.title}</Text>
-        <TouchableOpacity
-          style={styles.deleteButton}
-          onPress={() => showAlertStore(item.id)}
-        >
-          <Text style={styles.deleteButtonText}>Eliminar</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  
-
-    const showAlertStore = (id) =>{
-      Alert.alert(
-        "¿Estas seguro de eliminar la tarea?",
-        "Pulsa una opcion",
-        [
-          {text: "Cancelar"},
-          {text: "Confirmar", onPress: () =>  eliminarTarea(id)}
-        ],
-        { cancelable: true}
-      );
+        if (!querySnapshot.empty) {
+          const fetchedData = querySnapshot.docs.map(item => ({ id: item.id, ...item.data() }));
+          setData(fetchedData);
+        } else {
+          console.log('No se encontraron documentos en la colección.');
+        }
+      } catch (error) {
+        console.error('Error al obtener los documentos: ', error);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
-  
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.title}>Eliminar Tareas</Text>
+    mostrarAlumnos();
+  }, []);
+
+  return (
+    <View contentContainerStyle = {styles.container}>
+      {isLoading ? (
+        <Text>Cargando...</Text>
+      ) : (
+        <ScrollView contentContainerStyle={styles.datos}>
+      {alumnosArray.map((alumno, index) => (
+        <View key={index} style={styles.elementoList}>
+           <DatosAlumnos alumno={alumno} navigation={navigation} />
         </View>
-          <FlatList
-            data={tasks}
-            renderItem={ MostrarTarea } 
-            KeyExtractor = {task => task.id.toString()}   
-          />
-      </View>
-    );
-  }
+      ))}
+    </ScrollView>
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
+  datos: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  elementoList: {
+    flexDirection: 'column',
+    width:'50%',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     padding: 20,
-  },
-  header: {
-    marginBottom: 20,
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  taskItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderColor: 'lightgrey',
-    paddingVertical: 10,
-  },
-  deleteButton: {
-    backgroundColor: 'red',
-    padding: 8,
-    borderRadius: 5,
-  },
-  deleteButtonText: {
-    color: 'white',
-  },
+  }
 });
+
+export default TuComponente;
