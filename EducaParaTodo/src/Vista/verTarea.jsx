51918c4d5fa8da7711f9d1b81614c1608cb57d3d
Juan Marcos Sánchez Delgado <101177239+MARCOSD4@users.noreply.github.com>
2023-11-18 17:06:57 +0100

    Ver tarea modificado

--- EducaParaTodo/src/Vista/verTarea.jsx
+++ EducaParaTodo/src/Vista/verTarea.jsx
@@ -1,75 +1,179 @@
-// import React, { useState} from 'react';
-// import Constants from 'expo-constants';
-// import { View, Text, StyleSheet, FlatList } from 'react-native';
-// // import { CheckBox } from 'react-native-elements';
+ import React, { useState, useEffect} from 'react';
+ import Constants from 'expo-constants';
+ import { View, Text, StyleSheet, Button, Image, TouchableOpacity } from 'react-native';
+ import tareas from '../Modelo/tareas';
+ import { Entypo } from '@expo/vector-icons';
+//  import { CheckBox } from 'react-native-elements';
 
 
-// export function VerTarea ({nombreTarea, descripcion, pasos}){
-//     const [isChecked, setIsChecked] = useState(false);
+export function VerTarea (){
+    //const [isChecked, setIsChecked] = useState(false);
+    const [pasoActual, setPasoActual] = useState(0);
 
-//     const toggleCheck = () => {
-//         setIsChecked(!isChecked);
-//     };
+    useEffect(() => {
+        setPasoActual(0);
+    }, []);
 
-//     return (
-//         <FlatList
-//             data={[{ key: 'content' }]}
-//             renderItem={() => (
-//                 <View style={styles.container}>
-//                     <Text style={styles.tarea}>{nombreTarea}</Text>
-//                     <Text style={styles.descripcion}>{descripcion}</Text>
-//                     <Text style={styles.pasos}>Pasos a seguir:</Text>
+    /*const toggleCheck = () => {
+        setIsChecked(!isChecked);
+    };*/
 
-//                     <FlatList style={{padding: 20}}   
-//                         data={pasos}
-//                         renderItem={({item}) => 
-//                         <View style={{padding: 0}}>
-//                             <Text style={styles.title}>{item.title}</Text>
-//                             <Text style={styles.data}>{item.data}</Text>
-//                         </View>                
-//                         }
-//                     />
+    const datos = tareas();
+    //const tareasArray = Object.values(datos);     
 
-//                     <CheckBox textStyle={styles.check} 
-//                         title="Marca la casilla para completar la tarea"
-//                         checked={isChecked}
-//                         onPress={toggleCheck}
-//                     />
-//                 </View>                
-//             )}
-//         />              
-//     )
-// }
+    const handleSiguiente = () => {
+        if (pasoActual < datos[2].length - 1) setPasoActual(pasoActual + 1);
+    };
 
-// const styles = StyleSheet.create({
-//     container: {
-//         marginTop: Constants.statusBarHeight
-//     },
-//     tarea: {
-//         textAlign: 'center',
-//         fontWeight: 'bold', 
-//         fontSize: 30
-//     },
-//     descripcion: {
-//         fontSize: 17, 
-//         padding: 20, 
-//         textAlign: 'center'
-//     },
-//     pasos: {
-//         left: 30, 
-//         fontSize: 25, 
-//         fontWeight: 'bold', 
-//         paddingTop: 20
-//     },
-//     title: {
-//         fontSize: 20, 
-//         textDecorationLine: 'underline'
-//     },
-//     data: {
-//         fontSize: 17, padding: 20
-//     },
-//     check: {
-//         fontSize: 20, 
-//         textAlign: 'center'
-//     }
-// })
+    const handleAnterior = () => {
+        if (pasoActual > 0) setPasoActual(pasoActual - 1);
+    };
+
+    const pasosTarea = datos[2];
+    const pasoActualData = pasosTarea[pasoActual];
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.tarea}>{datos[0]}</Text>
+            {/*<Text style={styles.descripcion}>{datos[1]}</Text>*/}
+
+            <View style={styles.pasos}>
+                <Text style={styles.texto}>{pasoActualData.title}</Text>
+                <Image source={pasoActualData.imagen} style={{ width: 300, height: 300 }} />
+                <Text style={styles.texto}>{pasoActualData.data[0]}</Text>
+
+                <View style={styles.botonesContainer}>
+                        {pasoActual > 0 && (
+                            <TouchableOpacity onPress={handleAnterior} style={styles.botonAnterior}>
+                                <Entypo name="arrow-long-left" size={36} color="black" />
+                                <Text style={styles.botonTexto}>Anterior</Text>
+                            </TouchableOpacity>
+                        )}
+                        <TouchableOpacity onPress={handleSiguiente} style={styles.botonSiguiente}>
+                            <Entypo name="arrow-long-right" size={36} color="black" />
+                            <Text style={styles.botonTexto}>Siguiente</Text>
+                        </TouchableOpacity>
+                    </View>
+            </View>
+            
+            <View style={{ alignItems: 'center' }}>
+                <Text style={styles.opciones}>Opciones:</Text>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+                    <View style={{marginHorizontal: 1}}>
+                        <Button
+                            // onPress={onPressLearnMore}
+                            title="Texto"
+                            color="#509594"
+                        />
+                    </View>
+                    <View style={{marginHorizontal: 1}}>
+                        <Button
+                            // onPress={onPressLearnMore}
+                            title="Imágenes"
+                            color="#509594"
+                        />
+                    </View>
+                    <View style={{marginHorizontal: 1}}>
+                        <Button
+                            // onPress={onPressLearnMore}
+                            title="Vídeo"
+                            color="#509594"
+                        />
+                    </View>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginVertical: 10 }}>
+                    <View style={{marginHorizontal: 1}}>
+                        <Button
+                            // onPress={onPressLearnMore}
+                            title="Pictogramas"
+                            color="#509594"
+                        />
+                    </View>
+                    <View style={{marginHorizontal: 1}}>
+                        <Button
+                            // onPress={onPressLearnMore}
+                            title="Audio"
+                            color="#509594"
+                        />
+                    </View>
+                </View>
+            </View>
+
+            
+
+            {/*<Text style={styles.pasos}>Pasos a seguir:</Text>
+
+            <FlatList style={{padding: 20}}   
+                data={pasos}
+                renderItem={({item}) => 
+                <View style={{padding: 0}}>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Text style={styles.data}>{item.data}</Text>
+                </View>                
+                }
+            />*/}
+
+            {/*<CheckBox textStyle={styles.check} 
+                title="Marca la casilla para completar la tarea"
+                checked={isChecked}
+                onPress={toggleCheck}
+            />*/}
+        </View>                
+    )   
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: Constants.statusBarHeight,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    tarea: {
+        textAlign: 'center',
+        fontWeight: 'bold', 
+        fontSize: 40
+    },
+    pasos: {
+        alignItems: 'center',
+    },
+    texto:{
+        fontSize: 20, 
+        marginTop: Constants.statusBarHeight
+    },
+    botonesContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 20,
+    },
+    botonAnterior: {
+        alignItems: 'flex-start',
+        marginRight: 'auto'
+    },
+    botonSiguiente: {
+        alignItems: 'flex-end',
+        marginLeft: 'auto' 
+    },
+    botonTexto: {
+        textAlign: 'center',
+    },
+    opciones: {
+        fontSize: 30,
+        textAlign: 'center',
+    },
+    descripcion: {
+        fontSize: 17, 
+        padding: 20, 
+        textAlign: 'center'
+    },
+    title: {
+        fontSize: 20, 
+        textDecorationLine: 'underline'
+    },
+    data: {
+        fontSize: 17, padding: 20
+    },
+    check: {
+        fontSize: 20, 
+        textAlign: 'center'
+    }
+})
