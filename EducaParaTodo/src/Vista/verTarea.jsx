1b9bc4d55009b4a136beb7cbb019060b9d881432
Juan Marcos Sánchez Delgado <101177239+MARCOSD4@users.noreply.github.com>
2023-11-23 09:58:05 +0100

    un cambio

--- EducaParaTodo/src/Vista/verTarea.jsx
+++ EducaParaTodo/src/Vista/verTarea.jsx
@@ -146,6 +146,7 @@ const styles = StyleSheet.create({
     opciones: {
         fontSize: 30,
         textAlign: 'center',
+        marginBottom: 10
     },
     descripcion: {
         fontSize: 17, 
